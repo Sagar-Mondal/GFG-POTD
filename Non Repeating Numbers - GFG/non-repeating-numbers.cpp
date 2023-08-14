e0994f6f1806f9eea4db99e527f0f6c1c2ec2ca1
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2023-08-14 08:40:07 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        vector<int> res;
+        map<int,int> hashmap; //key=elements and value=frequency
+        for(auto it:nums){
+            hashmap[it]++;              
+        }
+        for(auto pair:hashmap){
+            if(pair.second==1){           // frequency is one
+                res.push_back(pair.first);    // add that key to res vector
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
