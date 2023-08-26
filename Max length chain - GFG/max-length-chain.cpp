e44eae739f2b67b8d09dc89d473c2735cad9ed7f
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2023-08-26 09:52:44 +0530

    Added solution - LeetHub

---
+++ Max length chain - GFG/max-length-chain.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct val{
+	int first;
+	int second;
+};
+
+
+// } Driver Code Ends
+/*
+The structure to use is as follows
+struct val{
+	int first;
+	int second;
+};*/
+
+class Solution{
+public:
+    bool static comp(val fst, val sec){
+        return fst.second<sec.second;
+    }
+    int maxChainLen(struct val p[],int n){
+        sort(p,p+n,comp);
+        int prev=-1e9;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(p[i].first>prev){
+                ans++;
+                prev=p[i].second;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		val p[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>p[i].first>>p[i].second;
+		}
+		Solution ob;
+		cout<<ob.maxChainLen(p,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
