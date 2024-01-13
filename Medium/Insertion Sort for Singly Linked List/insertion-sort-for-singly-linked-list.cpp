a02c379f07013bdc13a540475d5b153a92ce80e8
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-01-13 17:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* insertionSort(struct Node* head)
+    {
+        if(!head || !head->next) return head;
+        
+        Node* dummy=new Node(-1);
+        dummy->next=head;
+        Node* temp=head;
+        Node* it=head->next;
+        head->next=NULL;
+        
+        while(it){
+            Node* curr=it;
+            Node* next=curr->next;
+            curr->next=NULL;
+            
+            Node* prev=dummy;
+            while(temp){
+                if(temp->data >= curr->data) break;
+                temp=temp->next;
+                prev=prev->next;
+            }
+            prev->next=curr;
+            curr->next=temp;
+            
+            temp=dummy->next;
+            it=next;
+        }
+        
+        return dummy->next;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n;
+		cin >> n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < n; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+        Solution ob;
+
+		head = ob.insertionSort(head);
+		printList(head);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+// } Driver Code Ends
