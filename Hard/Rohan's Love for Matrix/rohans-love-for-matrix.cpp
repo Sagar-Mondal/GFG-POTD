9d56e4bf5a8cbd1dae12bf8bba039a840505e650
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-04-23 23:53:10 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        int first=1,second=1,ans=0;
+        if(n==1 || n==2) return first;
+        for(int i=0;i<n-2;i++){
+            ans=(first+second)%1000000007;
+            first=second;
+            second=ans;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
