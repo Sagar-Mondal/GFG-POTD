641a32f1704f5ca1a71c39d20183f63e58816334
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-05-26 09:55:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fruit Into Baskets/fruit-into-baskets.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalFruits(int N, vector<int> &fruits) {
+        int l = 0, r = 0, len = 0;
+        unordered_map<int,int> mp;
+        while(r < N){
+            mp[fruits[r]]++;
+            if(mp.size() > 2){
+                mp[fruits[l]]--;
+                if(mp[fruits[l]] == 0)  mp.erase(fruits[l]);
+                l++;
+            }
+            len = max(len, r - l + 1);
+            r++;
+        }
+        return len;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> fruits(N);
+        for (int i = 0; i < N; i++) cin >> fruits[i];
+        Solution obj;
+        cout << obj.totalFruits(N, fruits) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
