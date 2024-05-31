87b46866db613160681b56e6aca2da4921baf783
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-05-31 19:22:04 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setBit(int N)
+    {
+        for(int i = 0; i < 31; i++){
+            if((N & 1 << i) == 0){
+                return N | 1 << i;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.setBit(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
