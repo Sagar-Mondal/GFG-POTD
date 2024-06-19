0e4a6e06aeba9c0148e343e079f56384f698baf2
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-06-19 17:02:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size(), pre;
+        vector<int> cur(n + 1, 0);
+        for (int j = 1; j <= n; j++) {
+            cur[j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            pre = cur[0];
+            cur[0] = i;
+            for (int j = 1; j <= n; j++) {
+                int temp = cur[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    cur[j] = pre;
+                } else {
+                    cur[j] = min(pre, min(cur[j - 1], cur[j])) + 1;
+                }
+                pre = temp;
+            }
+        }
+        return cur[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
