7a3cc4a931edc46fd2459125a409712a110541c1
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-09-16 13:57:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -4,41 +4,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    int maxLength(string s){
+class Solution {
+  public:
+    int maxLength(string str) {
         stack<int> stk;
         stk.push(-1);
-        int maxL=0;
-        for(int i=0;i<s.size();i++)
-        {
-            int t=stk.top();
-            if(t!=-1&&s[i]==')'&&s[t]=='(')
-            {
+        int count=0;
+        for (int i=0;i<str.length();i++){
+            if(str[i]=='(') stk.push(i);
+            else{
                 stk.pop();
-                maxL=max(maxL,i-stk.top());
+                if(stk.empty()) stk.push(i);
+                int ans=i-stk.top();
+                count=max(count,ans);
             }
-            else
-                stk.push(i);
         }
-        return maxL;
+        return count;
+        
     }
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         string S;
-        cin>>S;
-        
+        cin >> S;
+
         Solution ob;
-        cout<<ob.maxLength(S)<<"\n";
+        cout << ob.maxLength(S) << "\n";
     }
     return 0;
 }
