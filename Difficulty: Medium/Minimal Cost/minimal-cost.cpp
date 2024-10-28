d98ef92d7ef879eebc962ed08a5cd73b28882978
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-10-28 17:23:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.cpp
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -8,16 +8,19 @@ class Solution {
   public:
     int minimizeCost(int k, vector<int>& arr) {
         int n = arr.size();
-    std::vector<int> dp(n, INT_MAX);
-    dp[0] = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 1; j <= k; ++j) {
-            if (i + j < n) {
-                dp[i + j] = std::min(dp[i + j], dp[i] + abs(arr[i] - arr[i + j]));
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+        for(int ind = 1; ind < n; ind++){
+            int mini = INT_MAX;
+            for(int j = 1; j <= k; j++){
+                if(ind - j >= 0){
+                    int energy = dp[ind - j] + abs(arr[ind] - arr[ind - j]);
+                    mini = min(mini, energy);
+                }
             }
+            dp[ind] = mini;
         }
-    }
-    return dp[n - 1];
+        return dp[n - 1];
     }
 };
 
@@ -42,6 +45,7 @@ int main() {
         Solution obj;
         int res = obj.minimizeCost(k, arr);
         cout << res << endl;
+        cout << "~" << endl;
         // string tl;
         // getline(cin, tl);
     }
