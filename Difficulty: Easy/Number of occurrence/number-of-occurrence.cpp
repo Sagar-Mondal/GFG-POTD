c1ee46205df41e0d8a1d5b6ee466a8c17fc16ecd
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2024-12-26 13:18:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -9,16 +9,40 @@ using namespace std;
 class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
-        unordered_map<int, int> mp;
-        for(auto it : arr){
-            mp[it]++;
-        }
-        for(auto it : mp){
-            if(it.first == target){
-                return it.second;
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int first = -1, last = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target){
+                first = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        return 0;
+        if(first == -1){
+            return 0;
+        }
+        low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target){
+                last = mid;
+                low = mid + 1;
+            }
+            else if(arr[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return (last - first + 1);
     }
 };
 
