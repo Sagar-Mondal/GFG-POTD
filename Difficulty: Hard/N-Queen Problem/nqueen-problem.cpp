e8984dc647df62e3f1091b89b6e9542d8a900d17
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2025-01-30 22:03:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,35 +4,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
+class Solution {
+  public:
     vector<vector<int>> result;
     int row[10], k = 0;
-    bool place(int r,int c) {
-    for(int prev=0;prev<c;prev++)
-    if(row[prev]==r or abs(row[prev]-r)==abs(prev-c))
-    return false;
-    return true;
+    bool place(int r, int c) {
+        for (int prev = 0; prev < c; prev++)
+            if (row[prev] == r or abs(row[prev] - r) == abs(prev - c))
+                return false;
+        return true;
     }
-    void bt(int c,int n) {
-    if(n == 2 or n == 3) 
-        return;
-    if(c == n) {
-    vector<int> v;
-    for(int i = 0;i < n;i++)
-        v.push_back(row[i]+1);
-    result.push_back(v);
-    k++;
-    }
-    for(int i = 0;i < n;i++) {
-    if(place(i, c)) {
-    row[c] = i; 
-    bt(c+1, n);
-    }
-    }
+    void bt(int c, int n) {
+        if (n == 2 or n == 3)
+            return;
+        if (c == n) {
+            vector<int> v;
+            for (int i = 0; i < n; i++)
+                v.push_back(row[i] + 1);
+            result.push_back(v);
+            k++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (place(i, c)) {
+                row[c] = i;
+                bt(c + 1, n);
+            }
+        }
     }
     vector<vector<int>> nQueen(int n) {
         result.clear();
@@ -43,26 +44,30 @@ public:
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
