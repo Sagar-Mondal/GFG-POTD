7aae56836c3392c504e89feda308630e3728a693
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2025-03-21 17:05:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -3,47 +3,56 @@
 using namespace std;
 typedef long long int ll;
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    int FindMaxSum(int arr[], int n)
-    {
-        vector<int> dp(n, -1);
-        dp[0] = arr[0];
-        for(int i = 1; i < n; i++){
-            int pick = arr[i];
-            if(i > 1)   pick += dp[i - 2];
-            int nonpick = dp[i - 1];
-            dp[i] = max(pick, nonpick);
+
+class Solution {
+  public:
+    int findMaxSum(vector<int>& arr) {
+        int n = arr.size();
+
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return arr[0];
+
+        // Set previous 2 values
+        int secondLast = 0, last = arr[0];
+
+        // Compute current value using previous two values
+        // The final current value would be our result
+        int res;
+        for (int i = 1; i < n; i++) {
+            res = max(arr[i] + secondLast, last);
+            secondLast = last;
+            last = res;
         }
-        return dp[n - 1];
+        return res;
     }
 };
 
+
 //{ Driver Code Starts.
-int main()
-{
-    //taking total testcases
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    //taking number of houses
-		int n;
-		cin>>n;
-		int a[n];
-		
-		//inserting money of each house in the array
-		for(int i=0;i<n;++i)
-			cin>>a[i];
-		Solution ob;
-		//calling function FindMaxSum()
-		cout<<ob.FindMaxSum(a,n)<<endl;
-	
-cout << "~" << "\n";
-}
-	return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findMaxSum(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
 
 // } Driver Code Ends
