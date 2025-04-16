2c7513f2deda6d46a293987df66e862cdf689137
Sagar Mondal <95748828+Sagar-Mondal@users.noreply.github.com>
2025-04-16 21:45:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int V = dist.size();
+
+        // Add all vertices one by one to
+        // the set of intermediate vertices.
+        for (int k = 0; k < V; k++) {
+
+            // Pick all vertices as source one by one
+            for (int i = 0; i < V; i++) {
+
+                // Pick all vertices as destination
+                // for the above picked source
+                for (int j = 0; j < V; j++) {
+
+                    // If vertex k is on the shortest path from
+                    // i to j, then update the value of dist[i][j]
+
+                    if (dist[i][k] != 100000000 && dist[k][j] != 100000000 &&
+                        dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.floydWarshall(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
